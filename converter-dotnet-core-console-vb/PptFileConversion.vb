2457fff9e68193d72a26af9a12766331ebd509d2
James A. Brannan <jamesabrannan@hushmail.com>
2025-04-04 15:30:54 -0400

    Update PptFileConversion.vb

--- converter-dotnet-core-console-vb/PptFileConversion.vb
+++ converter-dotnet-core-console-vb/PptFileConversion.vb
@@ -1,12 +1,34 @@
 ﻿Imports ceTe.DynamicPDF.Conversion
 
 Public Class PptFileConversion
-    Public Shared Sub Run(outputPdf As String)
+    Public Shared Sub Run()
+        SimplePowerpoint("ppt_conversion_output.pdf")
+        AdvancedPowerpoint("ppt_conversion_adv_output.pdf")
+    End Sub
 
-        Dim options As PowerPointConversionOptions = New PowerPointConversionOptions(False)
-        Dim powerPointConverter As PowerPointConverter = New PowerPointConverter(Program.GetResourcePath("DocumentA.pptx"), options)
-        powerPointConverter.Convert(Program.GetOutputDocPath(outputPdf))
+    Public Shared Sub SimplePowerpoint(outputPdf As String)
+        Dim options As New PowerPointConversionOptions(False)
+
+        Dim pptConverter As New PowerPointConverter(Program.GetResourcePath("DocumentA.pptx"), options)
+
+        pptConverter.Convert(Program.GetOutputDocPath(outputPdf))
+
         Console.WriteLine("OfficePptFileConversion Example Executed.")
+    End Sub
 
+    Public Shared Sub AdvancedPowerpoint(outputPdf As String)
+        Dim powerPointConversionOptions As New PowerPointConversionOptions(PageSize.A4, PageOrientation.Portrait, 36)
+
+        Dim handout As HandoutOutputType = PowerPointOutputType.Handout
+        handout.HandoutPrintOrder = HandoutPrintOrder.Horizontal
+        handout.SlidesPerPage = SlidesPerPage.Six
+        handout.FrameSlides = True
+
+        powerPointConversionOptions.OutputType = handout
+        powerPointConversionOptions.OutputType.PrintHiddenSlide = True
+
+        Dim powerPointConverter1 As New PowerPointConverter(Program.GetResourcePath("DocumentA.pptx"), powerPointConversionOptions)
+        powerPointConverter1.Convert(outputPdf)
     End Sub
 End Class
+
