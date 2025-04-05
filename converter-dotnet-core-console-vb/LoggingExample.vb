c54fd54734cb1d29b0a39a0d1ff7c97c808f8c7f
James A. Brannan <jamesabrannan@hushmail.com>
2025-04-05 12:22:18 -0400

    Update LoggingExample.vb

--- converter-dotnet-core-console-vb/LoggingExample.vb
+++ converter-dotnet-core-console-vb/LoggingExample.vb
@@ -1,3 +1,17 @@
-﻿Public Class LoggingExample
+﻿Imports ceTe.DynamicPDF.Conversion
+Imports System
+
+Public Class LoggingExample
+
+    Public Shared Sub LogConfiguration()
+        ConversionOptions.Logging.Level = LogLevel.Debug
+        ConversionOptions.Logging.LogFilePath = "C:\temp\LogFile.txt"
+        //ConversionOptions.Logging.LogAction = AddressOf DelegateMethod
+    End Sub
+
+    Public Shared Sub DelegateMethod(identifier As LogIdentifier, logLevel As LogLevel, message As String)
+        Console.WriteLine(logLevel.ToString() & "|" & identifier.ToString() & "|" & message)
+    End Sub
 
 End Class
+
